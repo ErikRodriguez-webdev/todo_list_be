bb9eced1237a6054d6e6ff1f231bcefafd51c368
Erik Rodriguez <perhapserik@gmail.com>
2021-12-27 15:56:14 -0800

    • adds production build for knexfile

--- knexfile.js
+++ knexfile.js
@@ -12,30 +12,14 @@ module.exports = {
     },
   },
 
-  // staging: {
-  //   client: 'postgresql',
-  //   connection: {
-  //     database: 'my_db',
-  //     user:     'username',
-  //     password: 'password'
-  //   },
-  //   pool: {
-  //     min: 2,
-  //     max: 10
-  //   },
-  //   migrations: {
-  //     tableName: 'knex_migrations'
-  //   }
-  // },
-
-  // production: {
-  //   client: "sqlite3",
-  //   connection: {
-  //     filename: "./Database/app.db3",
-  //   },
-  //   useNullAsDefault: true,
-  //   migrations: {
-  //     directory: "./Database/migrations",
-  //   },
-  // },
+  production: {
+    client: "sqlite3",
+    connection: {
+      filename: "./Database/app.db3",
+    },
+    useNullAsDefault: true,
+    migrations: {
+      directory: "./Database/migrations",
+    },
+  },
 };
