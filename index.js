2ba94f44f8f48b99acdba751d5b71d84aeff945f
Erik Rodriguez <perhapserik@gmail.com>
2021-12-27 16:29:07 -0800

    • makes port dynamic

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const server = require("./Api/server.js");
-const PORT = 5001;
+const PORT = process.env.PORT || 5001;
 
 server.listen(PORT, () => {
   console.log(`Server running on localhost:${PORT}`);
