3047776d2d58f6bdb840048703bc788e578600a8
Erik Rodriguez <perhapserik@gmail.com>
2021-12-27 16:02:38 -0800

    • updates environment for heroku

--- Database/db-config.js
+++ Database/db-config.js
@@ -1,6 +1,8 @@
 const knex = require("knex");
 const config = require("../knexfile.js");
 
-const db = knex(config.development);
+const environment = procss.env.NODE_ENV || "development";
+
+const db = knex(config.environment);
 
 module.exports = db;
